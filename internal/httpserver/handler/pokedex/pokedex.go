75a28b5a06e8dbae4f0f9601e16eab6c1befd2b0
defry256 <defry.heryanto@hotmail.com>
2022-03-21 01:53:45 +0700

    update not found http status for get pokemon data

--- internal/httpserver/handler/pokedex/pokedex.go
+++ internal/httpserver/handler/pokedex/pokedex.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/defry256/pokemon-helper/internal/app"
+	"github.com/defry256/pokemon-helper/internal/errors"
 	"github.com/defry256/pokemon-helper/internal/httpserver/handler"
 	"github.com/defry256/pokemon-helper/internal/httpserver/response"
 	"github.com/gorilla/mux"
@@ -13,6 +14,9 @@ func GetPokedex(application *app.App) http.HandlerFunc {
 	return handler.Handle(func(w http.ResponseWriter, r *http.Request) error {
 		pokemoNname := mux.Vars(r)["pokemonName"]
 		data := application.Pokedex.GetPokedex(pokemoNname)
+		if data == nil {
+			return errors.NewNotFoundError("Pokemon not found")
+		}
 		response.WithData(w, http.StatusOK, data)
 		return nil
 	})

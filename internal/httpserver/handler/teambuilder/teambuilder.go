820bbb999bb9a2bc1bc41879fa05cd67caa7826b
defry256 <defry.heryanto@hotmail.com>
2022-03-21 23:02:06 +0700

    add type suggestion on teambuilder

--- internal/httpserver/handler/teambuilder/teambuilder.go
+++ internal/httpserver/handler/teambuilder/teambuilder.go
@@ -11,10 +11,15 @@ import (
 	"github.com/defry256/pokemon-helper/internal/pokemon"
 )
 
+const (
+	MAX_TEAM_NUMBER = 6
+)
+
 func SimulateTeam(application *app.App) http.HandlerFunc {
 	return handler.Handle(func(w http.ResponseWriter, r *http.Request) error {
 		type payload struct {
-			Pokemons []string `json:"pokemons"`
+			Pokemons           []string `json:"pokemons"`
+			WithTypeSuggestion bool     `json:"with_type_suggestion"`
 		}
 
 		var p *payload
@@ -23,26 +28,32 @@ func SimulateTeam(application *app.App) http.HandlerFunc {
 			return errors.NewBadRequestError(err.Error())
 		}
 
-		if len(p.Pokemons) > 6 {
+		if len(p.Pokemons) > MAX_TEAM_NUMBER {
 			return errors.NewBadRequestError("Team must not exceed 6 pokemons")
 		}
 
+		teamResponse := map[string]interface{}{}
 		pokemons := []*pokemon.PokemonData{}
 		for _, p := range p.Pokemons {
 			poke := application.Pokedex.GetPokedex(p)
 			pokemons = append(pokemons, poke)
 		}
+		teamResponse["pokemons"] = pokemons
 
 		coveredTypes, uncoveredTypes, err := application.TeamBuilder.CalculateTypeCoverage(p.Pokemons)
 		if err != nil {
 			return err
 		}
+		teamResponse["covered_types"] = coveredTypes
+		teamResponse["uncovered_types"] = uncoveredTypes
 
-		response.WithData(w, http.StatusOK, simulateTeamResponse{
-			Pokemons:       pokemons,
-			CoveredTypes:   coveredTypes,
-			UncoveredTypes: uncoveredTypes,
-		})
+		if p.WithTypeSuggestion {
+			suggestionCount := (MAX_TEAM_NUMBER - len(p.Pokemons)) * 2
+			suggestionTypes := application.TeamBuilder.CalculateSuggestedType(uncoveredTypes, suggestionCount)
+			teamResponse["suggestion_types"] = suggestionTypes
+		}
+
+		response.WithData(w, http.StatusOK, teamResponse)
 		return nil
 	})
 }
